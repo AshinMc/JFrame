dd1aa32d66113b047e2cd7f976b83a50416952d4
Ashin <ashinalwin3@gmail.com>
2025-02-06 14:06:47 +0100

    Learned Buttons, and a bit about listeners

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
 
@@ -14,7 +17,32 @@ public class Main {
         //close window when close button pressed
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        //adding a button
+        JButton button = new JButton("Click me!");
+
+        //adding a Panel
+        JLabel label = new JLabel("Waiting for you my lord");
+
+
+        //Add an action listener to the button
+        button.addActionListener((new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                label.setText("Heck Yes, Button!");
+            }
+        }));
+        //creating panel
+        JPanel panel = new JPanel();
+        //adding stuff to panel
+        panel.add(button);
+        panel.add(label);
+
+        //adding panel to frame
+        frame.add(panel);
+
         //show window
         frame.setVisible(true);
+
     }
 }
