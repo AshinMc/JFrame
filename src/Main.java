d3fa2e32441a34489c90c9a4761d9264f3cf24da
Ashin <ashinalwin3@gmail.com>
2025-02-07 11:30:31 +0100

    Learning Textfield, layouts(still doubtful)

--- src/Main.java
+++ src/Main.java
@@ -8,41 +8,60 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello, World!");
-        // creating a main window
-        JFrame frame = new JFrame("Hello, World!");
 
-        //setting window size
-        frame.setSize(300, 300);
-
-        //close window when close button pressed
+        // Creating main window
+        JFrame frame = new JFrame("Hello, Ashin!");
+        frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //adding a button
+        // Creating buttons
         JButton button = new JButton("Click me!");
+        JButton button2 = new JButton("Close!");
 
-        //adding a Panel
-        JLabel label = new JLabel("Waiting for you my lord");
+        //creating text field
+        JTextField textField = new JTextField(20);
 
 
-        //Add an action listener to the button
-        button.addActionListener((new ActionListener() {
+        // Adding a label
+        JLabel titleLabel = new JLabel("Waiting for your text", SwingConstants.CENTER);
+
+        // Action listener for button
+        button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                label.setText("Heck Yes, Button!");
+                String enteredText = textField.getText(); // Get text from field
+                titleLabel.setText("You entered: " + enteredText); // Display it in the label
             }
-        }));
-        //creating panel
-        JPanel panel = new JPanel();
-        //adding stuff to panel
-        panel.add(button);
-        panel.add(label);
+        });
 
-        //adding panel to frame
-        frame.add(panel);
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
 
-        //show window
+        // Creating main panel
+        JPanel mainPanel = new JPanel(new BorderLayout());
+
+        // Adding label to top
+        mainPanel.add(titleLabel, BorderLayout.NORTH);
+
+        // Setting button panel (FlowLayout to prevent stretching)
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+
+        // Adding buttons to button panel
+        buttonPanel.add(textField);
+        buttonPanel.add(button);
+        buttonPanel.add(button2);
+
+        // Adding button panel to main panel
+        mainPanel.add(buttonPanel, BorderLayout.CENTER);
+
+        // Adding main panel to frame
+        frame.add(mainPanel);
+
+        // Show window
         frame.setVisible(true);
-
     }
 }
